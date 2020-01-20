22b98bef82f4623925164e2d3308e0559989591d
McCormack, John <john.mccormack@accenture.com>
2020-01-20 15:17:50 -0500

    [Add] Navigation Infrastructure.

--- commonui/src/main/java/com/jdmccormack/commonui/BaseFragment.kt
+++ commonui/src/main/java/com/jdmccormack/commonui/BaseFragment.kt
@@ -5,8 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavDeepLinkBuilder
+import androidx.navigation.fragment.findNavController
+import com.jdmccormack.commonui.navigation.*
 
 abstract class BaseFragment : Fragment() {
 
@@ -38,6 +42,38 @@ abstract class BaseFragment : Fragment() {
             }
         }
         return ViewModelProvider(this, factory).get(V::class.java)
+    }
+
+    /**
+     * Sets up the fragment to listen for navigation events from a particular [viewModel]
+     */
+    protected fun attachNavigationListener(viewModel: NavigationViewModel) {
+        configureNavigationListener(viewModel.navigationLiveDataField)
+    }
+
+    /**
+     * Sets up the fragment to listen for navigation events from [navigationLiveDataField]
+     */
+    private fun configureNavigationListener(navigationLiveDataField: LiveNavigationField<NavigationEvent>) {
+        navigationLiveDataField.observe(viewLifecycleOwner, Observer { navigate(it) })
+    }
+
+    private fun navigate(event: NavigationEvent) {
+        when (event) {
+            is NavigationIdEvent -> findNavController().navigate(event.navId, event.navigationArguments)
+            is NavigationDeepLinkEvent -> {
+                val pendingIntent = NavDeepLinkBuilder(requireContext())
+                    .setGraph(findNavController().graph)
+                    .setDestination(event.navId)
+                    .createPendingIntent()
+                pendingIntent.send()
+            }
+            is NavigationBackEvent -> {
+                findNavController().navigate(event.navId, event.navigationArguments)
+                findNavController().popBackStack(event.navId, false)
+            }
+
+        }
     }
 
 }
